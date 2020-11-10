c9d85112f59c5d542a3274ec59757aeaf508ceb8
huxiaoyu <1163567625@qq.com>
2020-11-10 16:56:10 +0800

    修改音乐首页的代码

--- miniprogram/pages/playlist/playlist.js
+++ miniprogram/pages/playlist/playlist.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    bannerList: [
+      {
+        url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+      },
+      {
+        url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg'
+      },
+      {
+        url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg'
+      }
+    ],
+    playlist: [
+      {
+        _id: '001',
+        picUrl: 'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+        playCount: 222343,
+        name: '中国娃'
+      },
+      {
+        _id: '002',
+        picUrl: 'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+        playCount: 245678,
+        name: '张三的歌'
+      },
+      {
+        _id: '003',
+        picUrl: 'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+        playCount: 245678,
+        name: '自由飞翔'
+      }
+    ]
   },
 
   /**
