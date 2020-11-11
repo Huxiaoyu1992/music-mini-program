139f8409c3f19fccf9c677e5277bee11f7001ad4
huxiaoyu <1163567625@qq.com>
2020-11-12 00:07:30 +0800

    getPlaylist云函数的调试

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -2,15 +2,28 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
+const db = cloud.database()
+const rp = require('request-promise')
+const URL = 'http://musicapi.xiecheng.live/personalized'
 
+const collection = db.collection('playlist')
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  const list = await collection.get()
+  const playlist = await rp(URL).then(res => {
+    return JSON.parse(res).result
+  })
+  const newData = []
+  for (let i = 0; i < playlist.length; i++) {
+    await collection.add({
+      data: {
+        ...playlist[i],
+        createTime: db.serverDate()
+      }
+    }).then(res => {
+      console.log('插入成功')
+    }).catch(e => {
+      console.error('插入失败')
+    })
   }
 }
