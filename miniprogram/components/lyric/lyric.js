a7363b249c2f49f739611707cad662bc63178fa1
huxiaoyu <1163567625@qq.com>
2020-11-18 11:14:43 +0800

    添加歌词的样式

--- miniprogram/components/lyric/lyric.js
+++ miniprogram/components/lyric/lyric.js
@@ -11,9 +11,9 @@ Component({
     lyric: String
   },
 
-  observers: {
+  observers: { // 用于监听组件内properties和data的变化
     lyric(lrc) {
-      console.log(lrc)
+      this._parseLyric(lrc)
     }
   },
   
@@ -21,12 +21,37 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    lyricList: []
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    /**
+     * 解析歌词
+     * @param {*} str 
+     */
+    _parseLyric(lrcStr) {
+      // 通过换行符对字符串进行分割
+      let line =  lrcStr.split('\n')
+      const list = []
+      line.forEach((ele) => {
+        let time = ele.match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g)
+        if (time != null) {
+          let lrc = ele.split(time)[1]
+          let timeReg = time[0].match(/(\d{2,}):(\d{2})(?:\.(\d{2,3}))?/) //
+          // 把时间格式转化为秒
+          let time2Seconds = parseInt(timeReg[1]) * 60 + parseInt(timeReg[2]) + parseInt(timeReg[3]) / 1000
+          list.push({
+            lrc,
+            time: time2Seconds
+          })
+        }
+      })
+      this.setData({
+        lyricList: list
+      })
+    }
   }
 })
