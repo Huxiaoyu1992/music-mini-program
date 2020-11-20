5d5f3fbdec7dc1d963e101cebfa3d6a278288dd8
huxiaoyu <1163567625@qq.com>
2020-11-20 16:23:47 +0800

    在小程序端使用云函数，完成评论弹窗的功能

--- miniprogram/components/blog-control/blog-control.js
+++ miniprogram/components/blog-control/blog-control.js
@@ -1,11 +1,13 @@
 // components/blog-control/blog-control.js
+const db = wx.cloud.database()
+
 let userInfo = {}
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    blogId: String
   },
 
   externalClasses: [
@@ -58,6 +60,44 @@ Component({
         })
       })
     },
-    loginReject() {}
+    loginReject() {},
+    onInput(event) {
+     this.setData({
+       content: event.detail.value
+     })
+    },
+    onSend() {
+      // 评论
+      if (!this.data.content) {
+        wx.showModal({
+          title: '内容不能为空',
+          content: ''
+        })
+        return
+      }
+      // 存储
+      wx.showLoading({
+        title: '评论中',
+        mask: true
+      })
+      db.collection('blog-comment').add({
+        data: {
+          content: this.data.content,
+          createTime: db.serverDate(),
+          blogId: this.properties.blogId,
+          avatarUrl: userInfo.avatarUrl,
+          nickName: userInfo.nickName
+        }
+      }).then(res => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '评论成功'
+        })
+        this.setData({
+          commentShow: false,
+          content: ''
+        })
+      })
+    }
   }
 })
